c1b21e25d2137edc251d9a0b9e544d86ba7cf1c1
Sean Cassiere <sean.cassiere@gmail.com>
2020-11-26 02:28:43 +0530

    Colors in Server Console

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,9 +8,9 @@ const connectDB = async () => {
       useCreateIndex: true
     })
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
-  } catch (err) {
-    console.error(`Error: ${error.message}`)
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold)
     process.exit(1)
   }
 }
