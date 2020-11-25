c693916b0fe89b151a43bf0eb2afa8d4bd5bce8a
Sean Cassiere <sean.cassiere@gmail.com>
2020-11-26 02:56:32 +0530

    MongoDB Models

---
+++ backend/models/userModel.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, required: true, default: false },
+}, { timeStamps: true })
+
+const User = mongoose.model('user', userSchema)
+
+export default User
