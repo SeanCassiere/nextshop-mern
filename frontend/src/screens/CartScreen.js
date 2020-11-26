ed7b5173edf32ff5b0a4109ce24c4e748a844096
Sean Cassiere <sean.cassiere@gmail.com>
2020-11-27 02:39:36 +0530

    Cart Remove Item

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import Message from '../components/Message'
 
-import { addToCart } from '../actions/cartActions'
+import { addToCart, removeFromCart } from '../actions/cartActions'
 
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id
@@ -21,7 +21,7 @@ const CartScreen = ({ match, location, history }) => {
   }, [dispatch, productId, qty])
 
   const removeFromCartHandler = (id) => {
-    console.log('remove')
+    dispatch(removeFromCart(id))
   }
 
   const checkoutHandler = () => {
