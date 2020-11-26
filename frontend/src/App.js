d39268de454b278ae6efb62d54572a659f437a25
Sean Cassiere <sean.cassiere@gmail.com>
2020-11-27 01:57:09 +0530

    Cart Reducer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Footer from './components/Footer'
 
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Container>
           <Route path='/' component={HomeScreen} exact />
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer />
