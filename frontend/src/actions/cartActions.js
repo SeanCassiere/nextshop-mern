5308cc03245f34c2e71f0b5673276eae8c52042e
Sean Cassiere <sean.cassiere@gmail.com>
2020-11-27 02:33:26 +0530

    Cart functionality without Remove

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 
-export const addToCar = (id, qty) => async (dispatch, getState) => {
+export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`)
 
   dispatch({
