8ed6b2c7ba05ede4126cfea2b6dcceab84e22c88
Sean Cassiere <sean.cassiere@gmail.com>
2020-11-27 03:28:50 +0530

    Generating JWTs

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,8 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 
+import generateToken from '../utils/generateToken.js'
+
 // @desc Auth user & get token
 // @route POST /api/users/login
 // @access Public
@@ -15,7 +17,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     })
   } else {
     res.status(401)
