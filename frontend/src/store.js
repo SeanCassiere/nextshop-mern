31edfbda544471572478a2864715542c59c7c86a
Sean Cassiere <sean.cassiere@gmail.com>
2020-11-27 12:52:11 +0530

    User Iogin Reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,18 +10,27 @@ import {
 
 import { cartReducer } from './reducers/cartReducers'
 
+import { userLoginReducer } from './reducers/userReducers'
+
+// Combine Reducers
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
+
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 }
 
 const middleware = [thunk]
