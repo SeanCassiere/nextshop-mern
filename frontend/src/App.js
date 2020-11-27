0a2461a5d00bdb54e9ef5ad572dceb9eaf9b5478
Sean Cassiere <sean.cassiere@gmail.com>
2020-11-28 00:58:14 +0530

    Payment Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
+          <Route path='/payment/' component={PaymentScreen} />
           <Route path='/shipping/' component={ShippingScreen} />
           <Route path='/profile/' component={ProfileScreen} />
           <Route path='/register/' component={RegisterScreen} />
