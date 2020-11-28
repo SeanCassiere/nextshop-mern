8208fc8f1927aac537db538a4651332dfbfc8a37
Sean Cassiere <sean.cassiere@gmail.com>
2020-11-29 02:18:08 +0530

    Back-End Delete User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -114,4 +114,28 @@ const getUsers = asyncHandler(async (req, res) => {
   res.json(users)
 })
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers }
+// @desc Delete user by id for Admin
+// @route DELETE /api/users/:id
+// @access Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    await user.remove()
+    res.json({ message: 'User removed' })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+
+  res.json(user)
+})
+
+export {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+}
