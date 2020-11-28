44d9336191b4f0101e6f39e4c2f57940b85b95c8
Sean Cassiere <sean.cassiere@gmail.com>
2020-11-29 02:07:45 +0530

    UserList Auth Front-End

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,7 +33,7 @@ const App = () => {
           <Route path='/login/' component={LoginScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
-          <Route path='/admin/userlist/:id' component={UserListScreen} />
+          <Route path='/admin/userlist/' component={UserListScreen} />
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -8,15 +8,22 @@ import Loader from '../components/Loader'
 
 import { listUsers } from '../actions/userActions'
 
-const UserListScreen = (history) => {
+const UserListScreen = ({ history }) => {
   const dispatch = useDispatch()
 
   const userList = useSelector((state) => state.userList)
   const { loading, error, users } = userList
 
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
   useEffect(() => {
-    dispatch(listUsers())
-  }, [dispatch])
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers())
+    } else {
+      history.push('/login')
+    }
+  }, [dispatch, history, userInfo])
 
   const deleteHandler = () => {}
 
