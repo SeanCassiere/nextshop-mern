08091289cd19dc36dd41183f7303a55bceb7f370
Sean Cassiere <sean.cassiere@gmail.com>
2020-11-29 01:22:23 +0530

    Admin Middleware and GetUsers

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -28,4 +28,13 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 })
 
-export { protect }
+const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next()
+  } else {
+    res.status(401)
+    throw Error('Not authorized, must be an admin')
+  }
+}
+
+export { protect, isAdmin }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,12 +4,13 @@ import {
   registerUser,
   getUserProfile,
   updateUserProfile,
+  getUsers,
 } from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { protect, isAdmin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-router.route('/').post(registerUser)
+router.route('/').post(registerUser).get(protect, isAdmin, getUsers)
 router.post('/login', authUser)
 router
   .route('/profile')
