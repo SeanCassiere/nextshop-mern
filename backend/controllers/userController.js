ccd162d96991338ceda347c28f9e1e645fcb9ae6
Sean Cassiere <sean.cassiere@gmail.com>
2020-11-29 03:11:22 +0530

    Get and Edit User Back-End for Admin

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,7 +77,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-// @desc UPdate user profile
+// @desc Update user profile
 // @route PUT /api/users/profile
 // @access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
@@ -131,6 +131,47 @@ const deleteUser = asyncHandler(async (req, res) => {
   res.json(user)
 })
 
+// @desc Get user by ID for Admin
+// @route GET /api/users/:id
+// @access Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password')
+
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+// @desc Update user by Admin
+// @route PUT /api/users/:id
+// @access Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if (req.body.isAdmin !== undefined) {
+      user.isAdmin = req.body.isAdmin
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
 export {
   authUser,
   registerUser,
@@ -138,4 +179,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 }
