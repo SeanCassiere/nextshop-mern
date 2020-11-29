24561f6aaaa06d869dea5db68b4451ff1f1e2109
Sean Cassiere <sean.cassiere@gmail.com>
2020-11-29 17:42:47 +0530

    Back-End Delete Product by ID

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,4 +22,18 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 })
 
-export { getProducts, getProductById }
+// @desc Delete Product By Id
+// @route DELETE /api/products/:id
+// @access Private/Admin
+const deleteProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    await product.remove()
+    res.json({ message: 'Product removed' })
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+export { getProducts, getProductById, deleteProductById }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,11 +2,17 @@ import express from 'express'
 import {
   getProductById,
   getProducts,
+  deleteProductById,
 } from '../controllers/productController.js'
+
+import { protect, isAdmin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/').get(getProducts)
-router.route('/:id').get(getProductById)
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, isAdmin, deleteProductById)
 
 export default router
